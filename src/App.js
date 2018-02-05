5dc7a7047547ca2d75891e116880c481ea4dc023
Mats Gimming Myhre <mats@myh.re>
2018-02-05 21:19:37 +0100

    Added filter for "currentlyReading" shelf.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchPage from './SearchPage'
 import ListBooks from './ListBooks'
@@ -9,13 +9,22 @@ class BooksApp extends React.Component {
   // TODO: When component did mount, BooksAPI.getall
 
   state = {
-    // TODO: Add shelves state
+    books : []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then( (books) => {
+      this.setState({books})
+      console.log(this.state.books)
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={ListBooks} />
+        <Route exact path="/" render={ () => (
+          <ListBooks books={this.state.books} />
+        )} />
         <Route path="/search" component={SearchPage} />
       </div>
     )
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import Book from './Book'
 
 class ListBooks extends Component {
+  static PropTypes = {
+    books: PropTypes.array.isRequired
+  }
+
   render() {
+    const { books } = this.props
+
+
     return (
       <div className="list-books">
         <div className="list-books-title">
@@ -14,42 +23,13 @@ class ListBooks extends Component {
               <h2 className="bookshelf-title">Currently Reading</h2>
               <div className="bookshelf-books">
                 <ol className="books-grid">
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">To Kill a Mockingbird</div>
-                      <div className="book-authors">Harper Lee</div>
-                    </div>
-                  </li>
-                  <li>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="none" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">Ender's Game</div>
-                      <div className="book-authors">Orson Scott Card</div>
-                    </div>
-                  </li>
+                  {books.filter( (books) => { return books.shelf === "currentlyReading" })
+                        .map( (books) => {
+                          return (
+                            <Book book={books} key={books.id} />
+                          )
+                        })
+                  }
                 </ol>
               </div>
             </div>
